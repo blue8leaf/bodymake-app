e7261e03deb20bfd8cf339da80fa59b84558abc4
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-11-06 19:21:50 +0900

    show コントローラー

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -16,6 +16,10 @@ class ReportsController < ApplicationController
     end
   end
 
+  def show
+    @report = Report.find(params[:id])
+  end
+
   private
   def report_params
     params.require(:report).permit(:image, :registration_date, :body_weight, :body_fat, :body_bust, :body_arm, :body_west, :body_belly, :body_hip, :body_thigh, :body_calf, :body_ankle).merge(user_id: current_user.id)
