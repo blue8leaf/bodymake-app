8f5625b562aad5359a39437e450b2550736d5c04
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-11-06 16:56:10 +0900

    report モデル テーブル 作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :reports
   
   with_options presence: true do
     validates :nickname
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_06_073643) do
+ActiveRecord::Schema.define(version: 2021_11_06_075240) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,24 @@ ActiveRecord::Schema.define(version: 2021_11_06_073643) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "reports", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "registration_date", null: false
+    t.string "body_weight", null: false
+    t.string "body_fat"
+    t.string "body_bust"
+    t.string "body_arm"
+    t.string "body_west"
+    t.string "body_belly"
+    t.string "body_hip"
+    t.string "body_thigh"
+    t.string "body_calf"
+    t.string "body_ankle"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reports_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "body_height", null: false
@@ -52,4 +70,5 @@ ActiveRecord::Schema.define(version: 2021_11_06_073643) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "reports", "users"
 end
