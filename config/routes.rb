28bdc3b6826104bcb592bd8bc70234733abb9df7
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-11-07 12:40:36 +0900

    Revert "destroy ルーティング"
    This reverts commit 38e59f4278632ff390c4d075f1fc2f642614fb54.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "reports#index"
-  resources :reports, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :reports, only: [:index, :new, :create, :show, :edit, :update]
 end
