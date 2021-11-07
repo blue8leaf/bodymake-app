a5361f40e1e0efc01888bfaaa09d2afd8264351c
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-11-07 19:15:29 +0900

    Revert "search ルーティング"
    This reverts commit 169d8188a59f2e0d24338a49c3c600921337b89c.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "reports#index"
-  resources :reports do
-    collection do
-      get 'search'
-    end
-  end
+  resources :reports, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :users, only: :show
 end
