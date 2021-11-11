b454e7c4491c2fe9935998fe03a2ae63894f659d
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-11-11 13:07:11 +0900

    目標体重 表示

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,5 +1,6 @@
 class GraphsController < ApplicationController
   def index
     @reports = Report.where(user_id: current_user.id).includes(:user)
+    @objective_weight = current_user.objective_weight
   end
 end
