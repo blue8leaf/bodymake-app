449732b761cf54ba9ef6c181f676e8e0b7e5890a
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-11-13 16:29:36 +0900

    トップページ ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "reports#index"
+  root to: "tops#index"
   resources :reports, only: [:index, :new, :create, :show, :edit, :update]
   resources :users, only: :show
   resources :graphs, only: :index
+  resources :tops, only: :index
 end
