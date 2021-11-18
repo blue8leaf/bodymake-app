5ab4c32c054439d9ee4e87b1ed7da4d0af90fb6b
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-11-18 21:34:07 +0900

    Revert "sns_idをビューで扱う コントローラー"
    This reverts commit 41e82c19658204b43c33c266098ec1acbcd914a2.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,13 +9,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
    private
   
    def authorization
-    sns_info = User.from_omniauth(request.env["omniauth.auth"])
-    @user = sns_info[:user]
-
+    @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted? #ユーザー情報が登録済み、ログイン処理を行う
       sign_in_and_redirect @user, event: :authentication
     else #ユーザー情報が未登録、新規登録画面へ遷移
-      @sns_id = sns_info[:sns].id
       render template: 'devise/registrations/new'
     end
    end
