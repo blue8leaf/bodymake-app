02e955873ade1b6952a624082c09875907b28a48
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-11-18 20:20:29 +0900

    omniauth アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :reports
+  has_many :sns_credentials
   
   with_options presence: true do
     validates :nickname
