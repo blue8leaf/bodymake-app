c0049390ebb5c80766e7a4b2b1397d4a55281d6a
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-11-18 16:43:34 +0900

    devise 新規登録 ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'  
+  } 
   root to: "tops#index"
   resources :reports, only: [:index, :new, :create, :show, :edit, :update]
   resources :users, only: :show
   resources :graphs, only: :index
   resources :tops, only: :index
+
+  devise_scope :user do
+    get "signup", :to => "users/registrations#new"
+  end
 end
